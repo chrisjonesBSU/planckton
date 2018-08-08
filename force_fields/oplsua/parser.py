04d4b2d4aed37be93b645bb7ec7e094b0da1c551
Mike Henry <mikehenry@boisestate.edu>
2018-08-08 15:27:13 -0600

    almost have angles done, need to figure out if I want to have an factor of 2 in the K values

--- force_fields/oplsua/parser.py
+++ force_fields/oplsua/parser.py
@@ -2,11 +2,17 @@ from dataclasses import dataclass
 
 
 # opls.par header
+# Lines 2-434
 # 0   1  2    3          4        5       6+
 # TYP AN AT   CHARGE     SIGMA    EPSILON Misc
 # Type Atomic-Number Element Charge(?) Sigma(Å) Epsilon(kcal/mol)
 
-# openMM units -> nm, amu, kJ/mole, proton charge
+# opls.sb header
+# lines 2-178?
+# 0             1                 2      3+
+# classA-classB K (kcal/A**2-mol) r (Å)  Misc
+
+# openMM units -> nm, amu, kJ/mole, proton charge, kJ/mol/nm**2 (bond k), kJ/mol/radian**2 (angle k)
 
 ff_par = "oplsua.par"
 ff_par_HEADER = 2
@@ -25,6 +31,12 @@ class OPLSUA_type:
     epsilon: float
     mass: float = -1
 
+@dataclass(frozen=True)
+class HarmonicBond:
+    class_1: str
+    class_2: str
+    k: float
+    lenght: float
 
 def write_xml(xml_data, xml_name):
     with open(xml_name, "w") as f:
@@ -59,7 +71,28 @@ for line in ff_par_lines[ff_par_HEADER:ff_par_OPLS_TYPE_END]:
     new_opls_type = OPLSUA_type(opls_type, atomic_number, atomic_name, charge, sigma, epsilon)
     oplsua_list.append(new_opls_type)
 
-print(problem_lines)
+print(f"problem lines {problem_lines} \n")
+
+with open(ff_sb, "r") as f:
+    ff_sb_lines = f.readlines()
+
+for line in ff_sb_lines[2:50]:
+    raw_harmonic_bond_type = line.strip(" ").strip().split(" ")
+    harmonic_bond_array = list(filter(None, raw_harmonic_bond_type))
+    # In some cases, there is a space between two classes, ie C -CA this causes an
+    # issue when we split things up, so we check to see if we need to concatenate
+    # the first two items in our list
+    if "-" not in harmonic_bond_array[0]:
+        harmonic_bond_array[:2] = ["".join(harmonic_bond_array[:2])]
+    class_1, class_2 = harmonic_bond_array[0].split("-")
+    # We need to convert kcal/(Å**2 mol) to kJ/(nm**2 mol)
+    k = float(harmonic_bond_array[1]) * (4.184 * 100)
+    # We need to convert Å to nm
+    lenght = float(harmonic_bond_array[2]) / 10
+    new_harmonic_bond = HarmonicBond(class_1, class_2, k, lenght)
+    print(new_harmonic_bond)
+
+
 
 openMM_xml = "<ForceField>\n"
 # Atom Types
--- force_fields/oplsua/readme.md
+++ force_fields/oplsua/readme.md
@@ -3,3 +3,5 @@
 ## Conversion methods
 * To convert angstroms to nano meters, a conversion factor of 1/10 is used.
 * To convert kcal/mol to kJ/mol, a conversion factor of 4.184 is used.
+* To convert kcal/(Å**2 mol) to kJ/(nm**2 mol), a conversion factor of 4.184 * 100 is used.
+
