179bb5b9f6435dd0c5a2763aa52a761dbcb4fac0
Mike Henry <mikehenry@boisestate.edu>
2018-08-13 17:18:34 -0600

    added more masses, added some explantion is readme

--- force_fields/oplsua/parser.py
+++ force_fields/oplsua/parser.py
@@ -16,14 +16,18 @@ ff_sb_HARMONIC_ANGLE_START = 179
 ff_aa_dihedrial = "../oplsaa/oplsaa.par.edits"
 ff_aa_dihedrial_SKIP = 4038
 
-# Taken from https://pubs.acs.org/doi/pdf/10.1021/ja00214a001
+# Most Taken from https://pubs.acs.org/doi/pdf/10.1021/ja00214a001
+# C* (opls type 135) no idea
 UA_MASS = {"C": 12.011, "O": 15.9994, "N": 14.0067, "H": 1.008, "C2": 14.027,
            "CH": 13.019, "C3": 15.035, "CD": 12.0107, "O2": 15.9994, "N3": 14.0067,
            "H3": 1.008, "OH": 15.9994, "HO": 1.008, "SH": 32.06, "HS": 1.008,
            "S": 32.06, "NA": 14.0067, "NB": 14.0067, "CP": 12.011, "CF": 12.011,
            "CC": 12.011, "CG": 12.011, "CB": 12.011, "N2": 14.0067, "CA": 12.001,
            "OS": 15.9994, "CT": 12.011, "C4": 16.043, "C9": 14.027, "C8": 13.019,
-           "C7": 12.011}
+           "C7": 12.011, "Ar": 39.948, "Kr": 83.798, "Xe": 131.293, "OW": 15.9994,
+           "HW": 1.008, "NT": "14.0067", "H2": 1.008, "NE": 20.1797, "C*": -1,
+           "NC": 14.0067, "CI": 13.019, "CJ": 13.019, "HC": 1.008, "CM": 13.019,
+           "CQ": 13.019, "CK": 13.019, "P": 30.97376, "C1": 13.019}
 
 @dataclass(frozen=False)
 class OPLSUA_type:
@@ -106,8 +110,9 @@ for line in ff_par_lines[ff_par_HEADER:ff_par_OPLS_TYPE_END]:
     except IndexError:
         problem_lines.append(opls_param_array)
         continue
-    # Skip the ?? types
-    if atomic_name == "??":
+    # Skip the ?? and DU types
+    if any(_ in atomic_name for _ in ("??", "DU")):
+        problem_lines.append(opls_param_array)
         continue
     atomic_number = opls_param_array[1]
     charge = opls_param_array[3]
--- force_fields/oplsua/readme.md
+++ force_fields/oplsua/readme.md
@@ -27,3 +27,5 @@
 * `oplsua.xml` Has UA types, bonds, and angles but AA dihedrals from `oplsaa.par.edits`
 * Some atom types in `oplsua.par` are `??` The parsing code skips these types.
 * Dihedirals can have wild cards by specifying and empty class/type, but bonds and angles cannot. Will need to check to see if any wild cards in are in the xml.
+* Something to think about with the mass dictionary, are atom classes reused? I don't know.
+* Hmmm I do think there is reuse, the net effect is I'll be off by a mass of H (or two).
