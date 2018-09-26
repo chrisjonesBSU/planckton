9e4392bd77e8d1e3f6bb56aa6b980d7046942ee9
Mike Henry <mikehenry@boisestate.edu>
2018-09-26 11:51:59 -0600

    added units, double checking using https://github.com/choderalab/openmm-forcefields/blob/master/amber/ffxml/gaff.xml

--- compounds/parser.py
+++ compounds/parser.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
-#ff_params = "eh-idtbr-frcmod"
-ff_params = "all-frcmod"
+
+PI = 3.141592653589
 
 
 class AmberFFParams:
@@ -11,6 +11,29 @@ class AmberFFParams:
     dihedrals = []
     impropers = []
     lj = []
+
+
+def kcal2kJ(val):
+    return val * 4.184
+
+
+def ang2nm(val):
+    return val/10
+
+
+def amber_bond2openmm_bond(val):
+    # don't forget the factor of two between functional forms
+    # 100 is for 1/ang to 1/nm
+    return kcal2kJ(val) * (100 * 2)
+
+
+def amber_angle2openmm_angle(val):
+    # don't forget the factor of two between functional forms
+    return kcal2kJ(val) * 2
+
+
+def deg2rad(val):
+    return val * (PI / 180)
 
 
 def parse_mass(line):
@@ -28,6 +51,8 @@ def parse_bond(line):
         line[:2] = ["".join(line[:2])]
     classes = line[0].split("-")
     k, r0 = line[1:3]
+    # Unit conversions
+    k, r0 = amber_bond2openmm_bond(k), ang2nm(r0)
     new_bond = AmberBond(*classes, k, r0)
     print(new_bond)
 
@@ -39,6 +64,8 @@ def parse_angle(line):
         line[0] += line.pop(1)
     classes = line[0].split("-")
     k, theta0 = line[1:3]
+    # Unit conversions
+    k, theta0 = amber_angle2openmm_angle(k), deg2rad(theta0)
     new_angle = AmberAngle(*classes, k, theta0)
     print(new_angle)
 
@@ -50,7 +77,11 @@ def parse_dihedral(line):
         line[0] += line.pop(1)
     classes = line[0].split("-")
     devider, barrier, phase, periodicity = map(float, line[1:5])
+    # http://ambermd.org/doc12/Amber18.pdf#page=247&zoom=auto,-398,716
+    # Section 14.1.6 in amber18 manual
     k_eff = barrier/devider
+    # Unit conversions
+    k_eff, phase = kcal2kJ(k_eff), deg2rad(phase)
     new_dihedral = AmberDihedral(*classes, k_eff, phase, periodicity)
     print(new_dihedral)
 
@@ -63,6 +94,8 @@ def parse_improper(line):
     classes = line[0].split("-")
     barrier, phase, periodicity = map(float, line[1:4])
     k_eff = barrier
+    # Unit conversions
+    k_eff, phase = kcal2kJ(k_eff), deg2rad(phase)
     new_improper = AmberImproper(*classes, k_eff, phase, periodicity)
     print(new_improper)
 
@@ -71,6 +104,8 @@ def parse_lj(line):
     line = line.strip().split(" ")
     line = list(filter(None, line))
     amber_type, sigma, epsilon = line
+    # Unit conversions
+    sigma, epsilon = ang2nm(sigma), kcal2kJ(epsilon)
     new_amber_lj = AmberLJ(amber_type, sigma, epsilon)
     print(new_amber_lj)
 
@@ -157,6 +192,8 @@ def get_section(key, line, f):
 
 
 if __name__ == "__main__":
+    #ff_params = "eh-idtbr-frcmod"
+    ff_params = "all-frcmod"
     with open(ff_params) as f:
         line = f.readline()
         while line:
