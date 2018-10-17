7ab12685603f97c2c5753e09f07d9a6509ea2e15
Mike Henry <mikehenry@boisestate.edu>
2018-10-17 10:02:40 -0600

    this is good enough to start adding flow

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -1,13 +1,12 @@
 from planckton.init import Compound, Pack
 from planckton.sim import Simulation
+from planckton.compounds import COMPOUND_FILE
+from planckton.force_fields import FORCE_FIELD
 
-pcbm_file = "compounds/pcbm_typed.mol2"
-itic_file = "compounds/itic_typed.mol2"
-ff_file = "compounds/gaff_pcbm.4fxml"
-pcbm = Compound(pcbm_file)
-itic = Compound(itic_file)
 
-packer = Pack([pcbm, itic], ff_file=ff_file, n_compounds=[10,10], density=0.5)
-packer.pack()
-my_sim = Simulation("init.hoomdxml", kT=3.0, gsd_write=1e2, log_write=1e2, e_factor=0.5, n_steps=1e3)
-my_sim._continue()
+def test_simple_sim():
+    pcbm = Compound(COMPOUND_FILE["pcbm"])
+    packer = Pack(pcbm, ff_file=FORCE_FIELD["opv_gaff"], n_compounds=10, density=0.5)
+    packer.pack()
+    my_sim = Simulation("init.hoomdxml", kT=3.0, gsd_write=1e2, log_write=1e2, e_factor=0.5, n_steps=1e3)
+    my_sim.run()
