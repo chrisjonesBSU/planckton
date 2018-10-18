ba417a12f7191d9a1faf5652c3d1285c6ed4122d
Mike Henry <mikehenry@boisestate.edu>
2018-10-18 09:32:53 -0600

    set lower shrink time

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -6,9 +6,12 @@ from planckton.force_fields import FORCE_FIELD
 
 def test_simple_sim():
     pcbm = Compound(COMPOUND_FILE["pcbm"])
-    packer = Pack(pcbm, ff_file=FORCE_FIELD["opv_gaff"], n_compounds=10, density=0.5)
+    packer = Pack(pcbm, ff_file=FORCE_FIELD["opv_gaff"], n_compounds=1, density=0.01)
     packer.pack()
     my_sim = Simulation(
-        "init.hoomdxml", kT=3.0, gsd_write=1e2, log_write=1e2, e_factor=0.5, n_steps=1e3
+        "init.hoomdxml", kT=3.0, gsd_write=1e2, log_write=1e2, e_factor=0.5, n_steps=3e3, mode="gpu", shrink_time=1e3
     )
     my_sim.run()
+
+if __name__ == "__main__":
+    test_simple_sim()
